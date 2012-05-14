e79634269ad41a567e5307f942096726fcc82848
Troels Henriksen <athas@sigkill.dk>
2012-05-14 10:52:33 +0200

    Initial import.

---
+++ Control/Monad/Par/Scheds/Edi.hs
@@ -0,0 +1,34 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving, MultiParamTypeClasses #-}
+
+module Control.Monad.Par.Scheds.Edi
+ (
+  Par,
+  IVar,
+  runPar,
+ )
+where
+
+import Control.Monad
+import Control.Monad.Par.Class
+import Control.Monad.Trans
+import Control.Parallel.Eden.Edi hiding (fork)
+
+newtype Par a = Par { runPar :: IO a }
+  deriving (Monad, MonadIO)
+
+data IVar a = IVar { iVarChan :: ChanName' a
+                   , iVarVal  :: a }
+
+instance ParFuture IVar Par where
+  spawn m = do ivar <- new
+               fork $ put ivar =<< m
+               return ivar
+  spawn_ m = do ivar <- new
+                fork $ put_ ivar =<< m
+                return ivar
+  get ivar = iVarVal ivar `seq` return (iVarVal ivar)
+
+instance ParIVar IVar Par where
+  fork = Par . spawnProcessAt 0 . runPar
+  new = Par $ uncurry IVar `liftM` createC
+  put_ ivar = Par . sendWith rseq (iVarChan ivar)
