3bbc289b9bc0f51f7b2d068267f54df789880806
Troels Henriksen <athas@sigkill.dk>
2012-05-16 08:56:07 +0200

    Remove nonfunctional IVar implementation; put in Futures.

--- Control/Monad/Par/Scheds/Edi.hs
+++ Control/Monad/Par/Scheds/Edi.hs
@@ -3,22 +3,37 @@
 module Control.Monad.Par.Scheds.Edi
  (
   Par,
-  IVar,
+  Future,
+--  IVar,
   runPar,
  )
 where
 
+import Control.Concurrent
 import Control.Monad
 import Control.Monad.Par.Class
 import Control.Monad.Trans
+import Control.Parallel.Eden (Trans)
 import Control.Parallel.Eden.Edi hiding (fork)
 
 newtype Par a = Par { runPar :: IO a }
   deriving Monad
 
-data IVar a = IVar { iVarChan :: ChanName' a
-                   , iVarVal  :: a }
+{-data IVar a = IVar { iVarChan :: ChanName' a
+                   , iVarVal  :: a }-}
 
+newtype Future a = Future { unFuture :: a }
+
+instance ParFuture Future Par where
+  spawn m = Par $ do (c,x) <- createC
+                     spawnProcessAt 0 $ sendNF c =<< runPar m
+                     return $ Future x
+  spawn_ m = Par $ do (c,x) <- createC
+                      spawnProcessAt 0 $ sendWith rseq c =<< runPar m
+                      return $ Future x
+  get = return . unFuture
+
+{-
 instance ParFuture IVar Par where
   spawn m = do ivar <- new
                fork $ put ivar =<< m
@@ -29,6 +44,9 @@ instance ParFuture IVar Par where
   get ivar = iVarVal ivar `seq` return (iVarVal ivar)
 
 instance ParIVar IVar Par where
-  fork = Par . spawnProcessAt 0 . runPar
+  fork m = Par $ do
+             spawnProcessAt 0 $ runPar m
+             return ()
   new = Par $ uncurry IVar `liftM` createC
   put_ ivar = Par . sendWith rseq (iVarChan ivar)
+-}
